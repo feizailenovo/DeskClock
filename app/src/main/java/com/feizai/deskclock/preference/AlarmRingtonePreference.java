65cffafb7931c4e3fb5e149c5eff4d0e0a948c32
feizai <c1376479276@163.com>
2022-03-26 12:49:38 +0800

    添加闹铃Preference

---
+++ app/src/main/java/com/feizai/deskclock/preference/AlarmRingtonePreference.java
@@ -0,0 +1,53 @@
+package com.feizai.deskclock.preference;
+
+import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.preference.RingtonePreference;
+import android.util.AttributeSet;
+
+import com.feizai.deskclock.R;
+
+/**
+ * Author: chenhao
+ * Date: 2022年3月26日 0026 下午 12:46:45
+ * Describe:
+ */
+public class AlarmRingtonePreference extends RingtonePreference {
+    private Uri mAlert;
+
+    public AlarmRingtonePreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onSaveRingtone(Uri ringtoneUri) {
+        setAlert(ringtoneUri);
+    }
+
+    @Override
+    protected Uri onRestoreRingtone() {
+        if (RingtoneManager.isDefault(mAlert)) {
+            return RingtoneManager.getActualDefaultRingtoneUri(getContext(),
+                    RingtoneManager.TYPE_ALARM);
+        }
+        return mAlert;
+    }
+
+    public void setAlert(Uri alert) {
+        mAlert = alert;
+        if (alert != null) {
+            final Ringtone r = RingtoneManager.getRingtone(getContext(), alert);
+            if (r != null) {
+                setSummary(r.getTitle(getContext()));
+            }
+        } else {
+            setSummary(R.string.silent_alarm_summary);
+        }
+    }
+
+    public Uri getAlert() {
+        return mAlert;
+    }
+}
